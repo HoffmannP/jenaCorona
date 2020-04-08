ce05c25c12a8b95709ce30c0298ffb908e24a682
Peter Hoffmann <Hoffmann.P@gmx.net>
2020-04-08 09:21:03 +0200

    Mit Änderungsrate und umbenannt

--- diagram.js
+++ diagram.js
@@ -1,188 +1,199 @@
 /* globals d3 */
 
 import * as shared from './shared.js'
-import * as lib from './lib.js'
 import de_DE from './local.de_DE.js'
 
 export const margin = {
-  left: 43,
-  top: 18,
-  right: 47,
-  bottom: 112
+  left: 45,
+  top: 45,
+  right: 45,
+  bottom: 85
 }
 export const width = shared.overallWidth - margin.left - margin.right
 export const height = shared.overallHeight - margin.top - margin.bottom
 
-const innerMargin = 15
-const squareWidth = 5
+const alpha = 0.5
 
-const styles = [
-  { color: '#ff420e', dot: 'rect' },
-  { color: '#004586', dot: 'diamond' },
-  { color: '#000000', dot: 'triangle' },
-  { color: '#24a121', dot: 'dot' }
+const categories = [
+  { name: 'Genesen', key: 'genesene', color: `rgba(  0, 128,   0, ${alpha})` },
+  { name: 'Tot', key: 'tote', color: `rgba(0,     0,   0, ${alpha})` },
+  { name: 'Infiziert', key: 'infizierte', color: `rgba(  0,   0, 255, ${alpha})` },
+  { name: 'Stationär', key: 'stationaer', color: `rgba(255, 165,   0, ${alpha})` },
+  { name: 'Schwerer Verlauf', key: 'schwerer_verlauf', color: `rgba(255,   0,   0, ${alpha})` }
 ]
 
-function cleanData (row) {
-  return {
-    date: 1000 * row.zeit,
-    erkrankte: +row.erkrankte,
-    genesene: +row.genesene,
-    tote: +row.tote
-  }
-}
-
-const formatPercent = x => `${Math.round(100 * x)} %`
+const toPercent = p => `${Math.round(100 * p)} %`
+const toPercentDez = p => `${Math.round(1000 * p) / 10} %`
 
 d3.timeFormatDefaultLocale(de_DE)
-const toDate = u => d3.timeFormat('%a, %e. %b')(new Date(u))
+const toDate = u => d3.timeFormat('%e.%m.')(new Date(u))
 
 const url = 'offiziell.csv'
 const svg = d3.create('svg')
+  .style('font-family', 'sans-serif')
 
-d3.csv(url, cleanData).then(data => {
-  data = data.slice(1)
-  let prev
-  data = data.map(d => {
-    const now = d.erkrankte // - d.tote - d.genesene
-    const v = { ...d, veränderung: now / prev - 1 }
-    prev = now
-    return v
-  })
+svg.node()
 
+d3.csv(url, row => ({
+  zeit: 1000 * row.zeit,
+  tote: -row.tote || -1e-9,
+  schwerer_verlauf: (+row.schwerer_verlauf) || 1e-9,
+  stationaer: (+row.stationaer - +row.schwerer_verlauf) || 1e-9,
+  genesene: -row.genesene,
+  infizierte: +row.erkrankte - +row.genesene - +row.stationaer - +row.tote,
+  aktiv: +row.erkrankte
+})).then(data => {
   svg
     .attr('width', shared.overallWidth)
     .attr('height', shared.overallHeight)
     .attr('viewBox', [-margin.left, -margin.top, shared.overallWidth, shared.overallHeight])
     .attr('xmlns', 'http://www.w3.org/2000/svg')
 
-  const dates = data.map(d => d.date)
-  const x = d3.scaleLinear().domain([d3.min(dates), d3.max(dates)]).range([innerMargin, width - innerMargin])
-  const y1 = d3.scaleLinear().domain([0, 200]).range([height, 0])
-  const y2 = d3.scaleLinear().domain([-0.3, 0.7]).range([height, 0])
+  // Transpose the data into layers
+  const stack = d3.stack()
+    .keys(categories.map(c => c.key))
+    .order(d3.stackOrderstackOrderNone)
+    .offset(d3.stackOffsetDiverging)
+  const datasets = stack(data)
 
-  const datasets = [
-    { name: 'erkrankte', axis: y1 },
-    { name: 'veränderung', axis: y2 },
-    { name: 'tote', axis: y1 },
-    { name: 'genesene', axis: y1 }
-  ]
+  // Calculate rate
+  const rate = data.map(
+    (v, i, d) => i > 0 ? { zeit: v.zeit, rate: v.aktiv / d[i-1].aktiv - 1 } : undefined
+  ).slice(1)
 
-  const xAxis = g => g
+  const newest = { ...data[data.length -1], rate: rate[rate.length - 1].rate }
+
+  const x = d3.scaleLinear().domain([d3.min(data, d => d.zeit), d3.max(data, d => d.zeit)]).range([0, width])
+  const y1 = d3.scaleLinear().domain([d3.min(data, d => d.genesene), d3.max(data, d => d.infizierte)]).nice().range([height, 0])
+  const y2 = d3.scaleLinear().domain([d3.min(rate, d => d.rate), d3.max(rate.slice(6), d => d.rate)]).nice().range([height, 0]).clamp(true)
+
+  const area = d3.area()
+    .x((d, i) => x(d.data.zeit))
+    .y0(d => y1(d[0]))
+    .y1(d => y1(d[1]))
+
+  svg.append('g').attr('class', 'x axis')
     .attr('transform', `translate(0,${height})`)
-    .call(d3.axisBottom(x)
-      .tickFormat(d => toDate(d))
-      .tickValues(data.map(d => d.date)))
-    .call(g => g.selectAll('.domain, .tick line')
-      .attr('stroke', '#bbb'))
-    .call(g => g.selectAll('.tick text')
-      .attr('text-anchor', 'end')
-      .attr('transform', 'rotate(-60) translate(-13, -8)'))
-
-  const y1Axis = g => g
-    .attr('transform', `translate(0,0)`)
-    .call(d3.axisLeft(y1))
-    .call(g => g.selectAll('.domain')
-      .attr('stroke', '#bbb'))
-    .call(g => g.selectAll('.tick line')
-      .attr('x2', width)
-      .attr('stroke', '#bbb'))
-    .call(g => g.select('.tick:last-of-type text').clone()
-      .attr('x', 3)
-      .attr('text-anchor', 'start')
-      .attr('font-weight', 'bold')
-      .text(data.erkrankte))
-
-  const y2Axis = g => g
-    .attr('transform', `translate(${width},0)`)
-    .attr('fill', 'grey')
-    .call(d3.axisRight(y2)
-      .tickFormat(formatPercent)
-      .tickValues([-0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]))
-    .call(g => g
-      .selectAll('.tick text')
-      .attr('color', (d, i) => d < 0 ? '#C00000' : 'default'))
-    .call(g => g.selectAll('.domain, .tick line')
-      .attr('stroke', '#bbb'))
-    .call(g => g.select('.tick:last-of-type text').clone()
-      .attr('x', 3)
-      .attr('text-anchor', 'end')
-      .attr('font-weight', 'bold')
-      .text(data.veränderung))
-
-  svg.append('g').call(xAxis)
-  svg.append('g').call(y1Axis)
-  svg.append('g').call(y2Axis)
+    .style('color', '#bbb')
+    .style('font-size', 13)
+    .call(d3.axisBottom(x).tickFormat(toDate))
+    .selectAll('text')
+    .attr('color', 'black')
 
   svg.append('g')
+    .attr('class', 'areas')
+    .selectAll('areas')
+    .data(datasets).enter()
+    .append('path')
+    .attr('fill', (d, i) => categories[i].color)
+    .attr('d', d => area(d))
+
+  svg.append('path')
     .attr('class', 'lines')
-    .selectAll('lines')
-    .data(datasets).enter()
-    .append('g')
-    .each((d, i, g) => {
-      const dotNode = d3.select(g[i])
-      lib.lineWithDot(dotNode, data, x, squareWidth, d, styles[i])
-    })
+    .datum(rate)
+    .attr('fill', 'none')
+    .attr('stroke', `rgb(128,   0, 128, ${(.5 + alpha/2)})`)
+    .attr('stroke-width', 2)
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-linecap', 'round')
+    .attr('d', d3.line()
+      .x(d => x(d.zeit))
+      .y(d => y2(d.rate)))
 
-  const firstUpper = text => `${text[0].toLocaleUpperCase()}${text.substring(1)}`
+  svg.append('rect')
+    .attr('x', -1)
+    .attr('y', -1)
+    .attr('height', 1)
+    .attr('width', width / 2)
+    .attr('fill', 'white')
 
-  svg.append('g')
-    .attr('class', 'legend')
-    .attr('transform', `translate(0, ${shared.overallHeight - 30})`)
-    .selectAll('g')
-    .data(datasets).enter()
-    .append('g')
-    .attr('transform', (d, i) => `translate(${[0, 1, 2.2, 3][i] * width / 4}, 0)`)
-    .call(g => g
-      .append('text')
-      .text(d => `${firstUpper(d.name)} (${(d.name === 'veränderung'
-        ? formatPercent
-        : x => x
-      )(data[data.length - 1][d.name])})`)
-      .attr('x', 4 * squareWidth)
-      .attr('font-family', 'sans-serif'))
-    .call(g => g
-      .append('path')
-      .attr('fill', 'none')
-      .attr('stroke', (d, i) => styles[i].color)
-      .attr('stroke-width', 3)
-      .attr('stroke-linejoin', 'round')
-      .attr('stroke-linecap', 'round')
-      .attr('d', `M0,-4l${3 * squareWidth},0`))
-    .each((d, i, g) => {
-      const dotNode = d3.select(g[i]).selectAll('dots.example').data([{ date: 1.5 * squareWidth, y: -4 }])
-      lib.dotfunc[styles[i].dot](dotNode, x => x, squareWidth, 'y', styles[i].color, y => y)
-    })
+  svg.append('g').attr('class', 'y1 axis')
+    .attr('transform', `translate(${width},0)`)
+    .style('color', '#bbb')
+    .style('font-size', 13)
+    .call(d3.axisRight(y1))
+    .call(g => g.selectAll('.tick line')
+      .attr('x2', -width))
+    .selectAll('text')
+    .attr('color', 'black')
 
-  svg.selectAll('text')
-    .attr('font-size', 13)
+  svg.append('g').attr('class', 'y2 axis')
+    .attr('transform', `translate(0,0)`)
+    .style('color', '#bbb')
+    .style('font-size', 13)
+    .call(d3.axisLeft(y2).tickFormat(toPercent))
+    .selectAll('text')
+    .attr('color', 'black')
 
   svg.append('text')
     .text('Coronafälle Jena')
-    .attr('font-family', 'sans-serif')
-    .attr('dominant-baseline', 'middle')
-    .attr('text-anchor', 'middle')
-    .attr('font-size', '18')
+    .style('font-size', 18)
+    .style('dominant-baseline', 'central')
+    .style('text-anchor', 'middle')
     .attr('x', width / 2)
-    .attr('y', margin.top)
+    .attr('y', -27)
 
-  svg.append('text')
-    .text('mit Steigungsrate')
-    .attr('font-family', 'sans-serif')
-    .attr('dominant-baseline', 'middle')
-    .attr('text-anchor', 'middle')
-    .attr('font-size', '15')
-    .attr('x', width / 2)
-    .attr('y', margin.top + height / 10)
+  const lw = width / categories.length
+  const ofst = 8
+  const offsetPos = [0, lw - ofst, 2 * lw - 2 * ofst, 3 * lw - 3 * ofst, 4 * lw - 4 * ofst]
+  const offsetWidth = [lw - ofst, lw - ofst, lw - ofst, lw - ofst, lw + 4 * ofst]
 
-  shared.disclaimer(d3, svg, new Date(data[data.length - 1].date))
+  svg.append('g')
+    .attr('class', 'legend')
+    .selectAll('entries')
+    .data([ categories[1], categories[0], ...categories.slice(2) ])
+    .enter().append('g')
+    .each((d, i, g) => d3.select(g[i])
+      .attr('transform', `translate(${offsetPos[i]}, ${height + margin.bottom / 2 - 9})`)
+      .call(g => g.append('rect')
+        .attr('width', offsetWidth[i])
+        .attr('height', 17)
+        .style('fill', d.color))
+      .call(g => g.append('text')
+        .attr('x', 3)
+        .attr('y', 3)
+        .text(`${d.name}: ${Math.abs(newest[d.key])}`)
+        .style('dominant-baseline', 'hanging')
+        .style('font-size', 13)
+        .style('fill', ['white', 'white', 'white', 'black', 'black'][i])))
+  svg.append('g')
+    .attr('class', 'legend')
+    .attr('transform', `translate(0, ${height + margin.bottom / 2 + 8})`)
+    .call(g => g.append('rect')
+      .attr('width', offsetWidth[0] + offsetWidth[1])
+      .attr('height', 17)
+      .style('fill', `rgb(128,   0, 128, ${alpha})`))
+    .call(g => g.append('text')
+      .attr('x', 3)
+      .attr('y', 3)
+      .text(`Änderungsrate: ${toPercentDez(newest.rate)}`)
+      .style('dominant-baseline', 'hanging')
+      .style('font-size', 13)
+      .style('fill', 'white'))
+  svg.append('g')
+    .attr('class', 'legend')
+    .attr('transform', `translate(${offsetPos[2]}, ${height + margin.bottom / 2 + 8})`)
+    .call(g => g.append('rect')
+      .attr('width', offsetWidth[2] + offsetWidth[3] + offsetWidth[4])
+      .attr('height', 17)
+      .style('fill', `rgba(255, 255,   0, ${alpha})`))
+    .call(g => g.append('text')
+      .attr('x', (offsetWidth[2] + offsetWidth[3] + offsetWidth[4]) / 2)
+      .attr('y', 3)
+      .text(`SARS-CoV-2 positiv getestet gesamt: ${newest.infizierte + newest.stationaer + newest.schwerer_verlauf}`)
+      .style('dominant-baseline', 'hanging')
+      .style('text-anchor', 'middle')
+      .style('font-size', 13)
+      .style('fill', 'black'))
+  
+  shared.disclaimer(d3, svg, new Date(newest.zeit))
     .attr('x', width / 2)
     .attr('y', -9)
 
   document.body.appendChild(svg.node())
   svg.node().style.backgroundImage = `url(${shared.bg})`
   if ((document.documentElement.clientWidth < shared.overallWidth) ||
-      (document.documentElement.clientHeight < shared.overallWidth)) {
+        (document.documentElement.clientHeight < shared.overallWidth)) {
     const factor = Math.min(
       document.documentElement.clientWidth / shared.overallWidth,
       document.documentElement.clientHeight / shared.overallHeight
@@ -197,10 +208,10 @@ d3.csv(url, cleanData).then(data => {
   }
 
   /*
-    const targetImg = document.createElement('img')
-    document.body.appendChild(targetImg)
-    .then(dataUrl => (targetImg.src = canvas.toDataUrl()))
-  */
+      const targetImg = document.createElement('img')
+      document.body.appendChild(targetImg)
+      .then(dataUrl => (targetImg.src = canvas.toDataUrl()))
+    */
 
   shared.diagramToFile(svg, { x: margin.left, y: margin.top, w: width, h: height })
     .then(canvas => {
